5cbbcc5d91d55b4f20d89239f7d0ac5da0758e15
thy3634 <thy3634@qq.com>
2024-09-23 23:06:20 +0800

    feat: change example into master-workers

--- src/main.ts
+++ src/main.ts
@@ -1,103 +1,50 @@
-/**
- * An example of using ChunkedUploader to upload a file, using multiple web workers to calc chunk's md5 in parallel.
- */
-import { ofetch } from "ofetch"
-import { ChunkedUploader } from "."
-import { hexStringToBase64 } from "./utils"
-import { md5 } from "hash-wasm"
-
 const form = document.querySelector('#form') as HTMLFormElement
 const fileInput = document.querySelector('#file-input') as HTMLInputElement
 const progress = document.querySelector('#progress') as HTMLProgressElement
 const chunkSizeInput = document.querySelector('#chunk-size-input') as HTMLInputElement
-const uploadButton = document.querySelector('#upload-button') as HTMLButtonElement
-
-/**
- * Create a web worker to calculate the md5 hash of a file.
- * @returns A object with three methods: `init`, `update`, `digest`.
- */
-function hashCreater() {
-    const worker = new Worker(new URL('hash.worker.ts', import.meta.url), { type: 'module' })
-    worker.addEventListener('error', console.error)
-    worker.addEventListener('messageerror', console.error)
-
-    return {
-        init() {
-            worker.postMessage({ type: 'init' })
-        },
-        update(data: Uint8Array | Uint16Array | Uint32Array | string) {
-            worker.postMessage({ type: 'update', data })
-        },
-        async digest() {
-            return new Promise<string>((resolve) => {
-                worker.postMessage({ type: 'digest' })
-                function onMessage(e: MessageEvent) {
-                    if (e.data.type === 'digest') {
-                        worker.removeEventListener('message', onMessage)
-                        resolve(e.data.hash)
-                    }
-                }
-                worker.addEventListener('message', onMessage)
-            })
-        }
-    }
-}
-
+const submitButton = document.querySelector('#submit-button') as HTMLButtonElement
 
 fileInput.addEventListener('change', async () => {
     const file = fileInput.files?.[0]
     if (file) {
         chunkSizeInput.disabled = true
-        uploadButton.disabled = false
-        const { id } = await ofetch<{ id: string }>('/chunked-upload', {
-            method: 'POST',
-            body: {
-                fileSize: file.size,
-                filename: file.name
+        const worker = new Worker(new URL('master.worker.ts', import.meta.url), { type: 'module' })
+
+        worker.postMessage({
+            type: 'init',
+            buffer: await file.arrayBuffer(),
+            name: file.name,
+            chunkSize: Number.parseInt(chunkSizeInput.value) * 1024 * 1024
+        })
+        submitButton.disabled = false
+        worker.addEventListener('message', (e) => {
+            switch (e.data.type) {
+                case 'progress': {
+                    progress.value = e.data.progress * 100
+                    break
+                }
+                case 'success': {
+                    submitButton.disabled = true
+                    alert('Upload complete')
+                    break
+                }
+                default: {
+                    console.error(e.data.error)
+                    submitButton.disabled = true
+                    alert('Upload failed')
+                    break
+                }
             }
         })
-
-        const uploader = new ChunkedUploader(file, async ({ index, blob, start, end }) => {
-            const formData = new FormData()
-            formData.append('file', blob)
-            return ofetch(`/chunked-upload/${id}/${index}`, {
-                method: 'POST',
-                body: formData,
-                headers: {
-                    'Range': `bytes=${start}-${end - 1}`,
-                    'Content-Digest': `md5=:${hexStringToBase64(await md5(new Uint8Array(await blob.arrayBuffer())))}:`
-                }
-            })
-        }, {
-            hashCreater,
-            chunkSize: Number.parseInt(chunkSizeInput.value) * 1024 * 1024,
-            // Set `options.limit` to limit the number of upload requests. 
-            // limit: navigator.hardwareConcurrency, // The number of CPU cores.
-        })
-
-        requestIdleCallback(() => uploadButton.disabled = false)
-
         form.addEventListener('submit', (e) => {
             e.preventDefault()
-            uploader.start()
-            uploadButton.disabled = true
-            chunkSizeInput.disabled = false
-        })
-        uploader.addEventListener('progress', () => {
-            progress.value = uploader.loaded / uploader.total * 100
-        })
-        uploader.addEventListener('success', () => {
-            alert('Upload complete')
-        })
-        uploader.addEventListener('error', (error) => {
-            console.error(error)
-            alert('Upload failed')
+            worker.postMessage({ type: 'start' })
         })
     }
 })
 
 form.addEventListener('reset', () => {
     progress.value = 0
-    uploadButton.disabled = true
+    submitButton.disabled = true
     chunkSizeInput.disabled = false
 })
--- src/master.worker.ts
+++ src/master.worker.ts
@@ -4,44 +4,90 @@ import { hexStringToBase64 } from './utils'
 
 const workerSelf = self as unknown as DedicatedWorkerGlobalScope
 
+let uploader: ChunkedUploader
+
 workerSelf.addEventListener('message', async (e: MessageEvent<{
     buffer: ArrayBuffer
     name: string
     chunkSize: number
     uploadedChunkIndexes?: number[]
+    limit?: number
+    type: 'init' | 'start'
 }>) => {
-    const { buffer, name, chunkSize, uploadedChunkIndexes } = e.data
-    const { id } = await ofetch('/chunked-upload', {
-        method: 'POST'
-    })
+    if (e.data.type === 'init') {
+        const { buffer, name, chunkSize, limit } = e.data
+        const { id } = await ofetch('/chunked-upload', {
+            method: 'POST',
+            body: {
+                fileSize: buffer.byteLength,
+                filename: name
+            }
+        })
 
-    const uploader = new ChunkedUploader({
-        buffer: buffer,
-        name,
-        size: buffer.byteLength,
-    }, async ({ buffer, index, start, end, hash }) => ofetch(`/chunked-upload/${id}/${index}`, {
-        method: 'PUT',
-        body: await buffer,
-        headers: {
-            'Content-Range': `bytes:${start}-${end - 1}`,
-            'Content-MD5': `md5:${hexStringToBase64(await hash)}:`,
-        }
-    }), {
-        chunkSize
-    })
+        uploader = new ChunkedUploader({
+            buffer: buffer,
+            name,
+            size: buffer.byteLength,
+        }, async ({ buffer, index, start, end, hash }) => ofetch(`/chunked-upload/${id}/${index}`, {
+            method: 'PUT',
+            body: await buffer,
+            headers: {
+                'Range': `bytes=${start}-${end - 1}`,
+                'Content-Digest': `md5=:${hexStringToBase64(await hash)}:`,
+            }
+        }), {
+            chunkSize,
+            hashCreater,
+            limit,
+        })
 
-    uploader.addEventListener('progress', (e) => {
-        postMessage({ type: 'progress', progress: e.loaded / e.total })
-    })
-    uploader.addEventListener('success', () => {
-        postMessage({ type: 'success' })
-        workerSelf.close()
-    })
-    uploader.addEventListener('error', () => {
-        console.error(uploader.error)
-        postMessage({ type: 'error', error: { name: uploader.error?.name, message: uploader.error?.message } })
-        workerSelf.close()
-    })
-
-    uploader.start(uploadedChunkIndexes)
+        uploader.addEventListener('progress', (e) => {
+            workerSelf.postMessage({ type: 'progress', progress: e.loaded / e.total })
+        })
+        uploader.addEventListener('success', () => {
+            workerSelf.postMessage({ type: 'success' })
+            workerSelf.close()
+        })
+        uploader.addEventListener('error', () => {
+            console.error(uploader.error)
+            workerSelf.postMessage({ type: 'error', error: { name: uploader.error?.name, message: uploader.error?.message } })
+            workerSelf.close()
+        })
+    }
+    else if (e.data.type === 'start') {
+        const { uploadedChunkIndexes } = e.data
+        uploader!.start(uploadedChunkIndexes)
+    }
 })
+
+/**
+ * Create a web worker to calculate the md5 hash of a file.
+ * @returns A object with three methods: `init`, `update`, `digest`.
+ */
+function hashCreater() {
+    const worker = new Worker(new URL('hash.worker.ts', import.meta.url), { type: 'module' })
+    worker.addEventListener('error', console.error)
+    worker.addEventListener('messageerror', console.error)
+
+    return {
+        init() {
+            worker.postMessage({ type: 'init' })
+        },
+        update(data: Uint8Array | Uint16Array | Uint32Array | string) {
+            worker.postMessage({ type: 'update', data })
+        },
+        async digest() {
+            return new Promise<string>((resolve) => {
+                worker.postMessage({ type: 'digest' })
+                function onMessage(e: MessageEvent) {
+                    if (e.data.type === 'digest') {
+                        worker.removeEventListener('message', onMessage)
+                        worker.terminate()
+                        resolve(e.data.hash)
+                    }
+                }
+                worker.addEventListener('message', onMessage)
+            })
+        }
+    }
+}
